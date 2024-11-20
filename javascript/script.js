fef65a68333ac4d264afee3c02ae57db41bf38d6
João Pedro dos Santos <jpedrosantosoliv@gmail.com>
2024-11-20 11:50:32 -0300

    finished fotos page

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-function showMenu(){
-    var menuHamburguer = document.getElementById("header-navigation");
-    if (menuHamburguer.style.display === "block"){
-        menuHamburguer.style.display = "none";
-    } else {
-        menuHamburguer.style.display = "block";
-    }
-}
