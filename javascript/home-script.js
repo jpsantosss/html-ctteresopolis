d4069aea6a7befca15126cd489596a984d72fcd7
João Pedro Santos <jpedrosantosoliv@gmail.com>
2025-01-23 11:31:38 -0300

    home responsivity
    created home responsivity for all components

--- javascript/home-script.js
+++ javascript/home-script.js
@@ -5,12 +5,15 @@ function changeBackground() {
 
     if (pageWidth < 600) {
         imageHome.style.backgroundImage = "none";
+        imageHome.style.height = "100%"
         imageHome.innerHTML = '<img src="images/home/comunicado-ctt-s.jpeg" alt="Comunicado">'
     } else if (pageWidth < 1130) {
         imageHome.style.backgroundImage = "url('../images/home/comunicado-ctt-m.jpeg')";
+        imageHome.style.height = "600px"
         imageHome.innerHTML = "";
     } else {
         imageHome.style.backgroundImage = "url('../images/home/comunicado-ctt-l.jpeg')";
+        imageHome.style.height = "600px"
         imageHome.innerHTML = "";
     }
 }
