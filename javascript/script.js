0e49c5e223bf387aa3b6d28224536a6d7d8018ed
João Pedro Santos <jpedrosantosoliv@gmail.com>
2025-02-04 17:47:32 -0300

    menu responsivity

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,15 @@
-/*Global Script*/
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".navigation-mobile i");
+    const menu = document.querySelector(".header-navigation");
+    const logo = document.querySelector(".header-logo");
+
+    // Função para mostrar/esconder o menu mobile
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
+
+    // Redireciona para o index ao clicar no logo
+    logo.addEventListener("click", function () {
+        window.location.href = "../index.html";
+    });
+});
