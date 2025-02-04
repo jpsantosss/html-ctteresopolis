9712b7097534c01effe71129d6edd4c84d6ccb50
João Pedro Santos <jpedrosantosoliv@gmail.com>
2025-02-04 20:27:29 -0300

    Update home-script.js

--- javascript/home-script.js
+++ javascript/home-script.js
@@ -5,17 +5,17 @@ function changeBackground() {
 
     if (pageWidth < 600) {
         imageHome.style.backgroundImage = "none";
-        imageHome.style.height = "100%"
-        imageHome.innerHTML = '<img src="images/home/comunicado-ctt-s.jpeg" alt="Comunicado">'
+        imageHome.style.height = "100%";
+        imageHome.innerHTML = '<img src="images/home/comunicado-ctt-s.jpeg" alt="Comunicado">';
     } else if (pageWidth < 1130) {
-        imageHome.style.backgroundImage = "url('./images/home/comunicado-ctt-m.jpeg')";
-        imageHome.style.height = "600px"
+        imageHome.style.backgroundImage = "url('images/home/comunicado-ctt-m.jpeg')";
+        imageHome.style.height = "600px";
         imageHome.innerHTML = "";
     } else {
-        imageHome.style.backgroundImage = "url('./images/home/comunicado-ctt-l.jpeg')";
-        imageHome.style.height = "600px"
+        imageHome.style.backgroundImage = "url('images/home/comunicado-ctt-l.jpeg')";
+        imageHome.style.height = "600px";
         imageHome.innerHTML = "";
-    }
+    }    
 }
 window.addEventListener('resize', changeBackground);
 
